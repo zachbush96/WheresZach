6e08f0eb9b4c6a258d54d566f5d382de74971260
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-05-05 01:57:32 -0400

    Update script.js
    adding body-parser and creating jsonParser and passing it to app.post()

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+//For parsing req.body()
+var jsonParser = bodyParser.json();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
  
 //Hosting index page
-app.use(express.static("landing-page"))
+app.use(express.static("landing-page"));
 app.get('/', (req,res) =>{
  res.sendFile('index.html', {root: __dirname + '/landing-pages'});
 });
 
 //Array that holds dicts that contain LAT and LONG cordinates for mapping later
-var previousLocations = []         //[{"lat":18.33333,"long":-21.22222},{"lat":19.5555,"long":-19.32323},{"lat":20.2,"long":19.6656}]
+var previousLocations = [];         //[{"lat":18.33333,"long":-21.22222},{"lat":19.5555,"long":-19.32323},{"lat":20.2,"long":19.6656}]
         
 
 
 //Listening for get requests
- app.post('/api/v1/location', (req, res) => { 
+ app.post('/api/v1/location', jsonParser ,(req, res) => { 
      console.log(req.body);
      //var cords = req.body.cords;
      //console.log("Got Post. Cords: " + cords);
@@ -23,5 +26,5 @@ var previousLocations = []         //[{"lat":18.33333,"long":-21.22222},{"lat":1
     }); 
 
 app.listen(PORT, () => {
-  console.log("Listending on port " + PORT)
+  console.log("Listending on port " + PORT);
 });
