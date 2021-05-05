fb00de85861132fa55fcec0231b8132a9cdaa192
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-05-05 04:23:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,9 @@ var previousLocations = [];         //[{"lat":18.33333,"long":-21.22222},{"lat":
  app.post('/api/v1/location', jsonParser ,(req, res) => { 
      console.log(req.body);
      var cords = req.body.cords;
-     cords = cords.split(":");
-     cords = cords.split(",");
+     console.log(typeof cords);
+     //cords = cords.split(":");
+     //cords = cords.split(",");
      console.log("Got Post. Cords: " + cords);
      var lat = cords[2];
      var long = cords[4];
