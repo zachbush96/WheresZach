b59978120859fe38ef6417bbe327738a9f7e5a79
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-05-05 04:19:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ var previousLocations = [];         //[{"lat":18.33333,"long":-21.22222},{"lat":
      cords.split(":");
      cords.split(",");
      console.log("Got Post. Cords: " + cords);
-     var lat = cords.split(",")[2];
-     var long = cords.split(",")[4];
+     var lat = cords[2];
+     var long = cords[4];
      console.log("lat: " + lat);
      console.log("long: " + long);
      res.send("200");
