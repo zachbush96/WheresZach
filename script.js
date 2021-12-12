05c9fe62c8b12d2132c0e192220104d96766139f
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-12-12 17:59:15 -0500

    Update script.js
    Adding Database Configs
    **working on desktop :/

--- script.js
+++ script.js
@@ -1,42 +1,87 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+fs = require('fs');
+const readline = require('readline');
+//connect to supabase database
+const supabase = require('@supabase/supabase-js');
+const res = require('express/lib/response');
 
+const supabaseClient = supabase.createClient('https://rlbquimdrbabjxsckgzy.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTM0MTU3MSwiZXhwIjoxOTU0OTE3NTcxfQ.g0tDGpW8nYAmAkUcIO-Pj9ielw_P4qu5DhqywhGI4Hc');
 var jsonParser = bodyParser.json();
-
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 3000;
- 
+
+
+
 //Hosting index page
 app.use(express.static("landing-page"));
 app.get('/', (req,res) =>{
  res.sendFile('index.html', {root: __dirname + '/landing-pages'});
 });
 
+
 //Array that holds dicts that contain LAT and LONG cordinates for mapping later
-var previousLocations = [];    //[{"lat":18.33333,"long":-21.22222},{"lat":19.5555,"long":-19.32323},{"lat":20.2,"long":19.6656}]
-        
+var previousLocations = [];         //[{"lat":18.33333,"long":-21.22222},{"lat":19.5555,"long":-19.32323},{"lat":20.2,"long":19.6656}]
+
+
+
+//Accepts json location object and saves it to supabase database
+function saveLocationsToDatabase(locationObject){
+  console.log("saving to DB");
+  console.log(locationObject);
+  supabaseClient
+  .from('Locations')
+  .insert(locationObject)
+  .then(response => {
+    console.log("Saved to DB");
+    console.log(response);
+  }).catch(err => {
+    console.log("Error saving to DB");
+
+  });
+
+};
+
+//Return locations from supabase database
+function getLocationsFromDatabase(){
+  console.log("getting from DB");
+  supabaseClient
+  .from('Locations')
+  .select('*')
+  .then(data => {
+    //console.log("Got from DB");
+    for(location in data.body){
+      locationObject = {'Lat':data.body[location].Lat, 'Long':data.body[location].Long};
+      previousLocations.push(locationObject);
+    }
+    return data.body;
+  }).catch(err => {
+    console.log("Error getting from DB");
+  });
+};
+getLocationsFromDatabase();
+
 
 
 //Listening for get requests
  app.post('/api/v1/location', jsonParser ,(req, res) => { 
-     console.log("Body --> "+req.body);
-     var cords = req.body.cords;
-     console.log("typeof cords -->" + typeof cords);
-     cords = cords.split(":");
-     console.log("Cords after being split : : "+typeof cords);
-     console.log(cords);
+    console.log("Body --> "+req.body);
+    var cords = req.body.cords;
+    console.log("typeof cords -->" + typeof cords);
+    cords = cords.split(":");
+    console.log("Cords after being split : : "+typeof cords);
+    console.log(cords);
 
-     //Setting lat and long variables
-     var lat = cords[1].slice(1,-8);
-     var long = cords[2].slice(1,-2);
-     console.log("lat: " + lat);
-     console.log("long: " + long);
+    //Setting lat and long variables
+    var lat = cords[1].slice(1,-8);
+    var long = cords[2].slice(1,-2);
+    console.log("lat: " + lat);
+    console.log("long: " + long);
      
-     previousLocations.push({"lat":lat,"long":long})
-
-     res.send("200");
+    previousLocations.push({"Lat":lat,"Long":long})
+    saveLocationsToDatabase({"Lat":lat,"Long":long});
+    
+    res.send("200");
     }); 
 
 app.get('/api/v1/locations', (req,res) => {
